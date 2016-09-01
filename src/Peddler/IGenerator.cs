4558a43175b184ea2e47a8b195f7a938532df708
Brian Caruso <carusobp@gmail.com>
2016-08-31 17:03:09 -0700

    Add XML documentation to interfaces

--- src/Peddler/IGenerator.cs
+++ src/Peddler/IGenerator.cs
@@ -2,8 +2,17 @@ using System;
 
 namespace Peddler {
 
+    /// <summary>
+    /// A common interface for generate values of type <typeparamref name="T"/>.
+    /// </summary>
     public interface IGenerator<out T> {
 
+        /// <summary>
+        /// Creates a new instance of <typeparamref name="T"/> based
+        /// upon the constraints injected into this implementation of
+        /// <see cref="T:Peddler.IGenerator`1"/>
+        /// </summary>
+        /// <returns>An instance of <typeparamref name="T"/></returns>
         T Next();
 
     }

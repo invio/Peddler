f7a40e5a0e8114426743230490adac50305e0995
Brian Caruso <carusobp@gmail.com>
2016-09-04 22:52:31 -0700

    Add missing DateTimeGenerator tests

--- test/Peddler.Tests/DateTimeGeneratorTests.cs
+++ test/Peddler.Tests/DateTimeGeneratorTests.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using Xunit;
 
 namespace Peddler {
 
@@ -18,6 +20,146 @@ namespace Peddler {
 
             return new DateTimeGenerator(low, high);
         }
+
+        [Fact]
+        public void Constructor_WithDefaults_KindIsUtc() {
+            var generator = new DateTimeGenerator();
+
+            Assert.Equal(DateTimeKind.Utc, generator.Kind);
+        }
+
+        public static IEnumerable<object[]> Constructor_WithLow_KindIsInferred_Data {
+            get {
+                var ticks = DateTime.Now.Ticks;
+
+                yield return new object[] { new DateTime(ticks, DateTimeKind.Unspecified) };
+                yield return new object[] { new DateTime(ticks, DateTimeKind.Local) };
+                yield return new object[] { new DateTime(ticks, DateTimeKind.Utc) };
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(Constructor_WithLow_KindIsInferred_Data))]
+        public void Constructor_WithLow_KindIsInferred(DateTime date) {
+            var generator = new DateTimeGenerator(date);
+
+            Assert.Equal(date.Kind, generator.Kind);
+        }
+
+        public static IEnumerable<object[]> Constructor_WithRange_ConsistentKinds_Data {
+            get {
+                var ticks = DateTime.Now.Ticks;
+
+                yield return new object[] {
+                    new DateTime(ticks, DateTimeKind.Unspecified),
+                    new DateTime(ticks + 1, DateTimeKind.Unspecified)
+                };
+
+                yield return new object[] {
+                    new DateTime(ticks, DateTimeKind.Local),
+                    new DateTime(ticks + 1, DateTimeKind.Local)
+                };
+
+                yield return new object[] {
+                    new DateTime(ticks, DateTimeKind.Utc),
+                    new DateTime(ticks + 1, DateTimeKind.Utc)
+                };
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(Constructor_WithRange_ConsistentKinds_Data))]
+        public void Constructor_WithRange_ConsistentKinds(DateTime low, DateTime high) {
+            var generator = new DateTimeGenerator(low, high);
+
+            Assert.Equal(low.Kind, generator.Kind);
+            Assert.Equal(high.Kind, generator.Kind);
+        }
+
+        public static IEnumerable<object[]> Constructor_WithRange_MismatchedKinds_Data {
+            get {
+                var ticks = DateTime.Now.Ticks;
+
+                yield return new object[] {
+                    new DateTime(ticks, DateTimeKind.Unspecified),
+                    new DateTime(ticks + 1, DateTimeKind.Local)
+                };
+
+                yield return new object[] {
+                    new DateTime(ticks, DateTimeKind.Local),
+                    new DateTime(ticks + 1, DateTimeKind.Utc)
+                };
+
+                yield return new object[] {
+                    new DateTime(ticks, DateTimeKind.Utc),
+                    new DateTime(ticks + 1, DateTimeKind.Unspecified)
+                };
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(Constructor_WithRange_MismatchedKinds_Data))]
+        public void Constructor_WithRange_MismatchedKinds(DateTime low, DateTime high) {
+            Assert.Throws<ArgumentException>(
+                () => new DateTimeGenerator(low, high)
+            );
+        }
+
+        [Fact]
+        public void NextDistinct_MismatchedKind() {
+            var date = DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Local);
+            var generator = new DateTimeGenerator();
+
+            Assert.NotEqual(date.Kind, generator.Kind);
+            Assert.Throws<ArgumentException>(
+                () => generator.NextDistinct(date)
+            );
+        }
+
+        [Fact]
+        public void NextLessThan_MismatchedKind() {
+            var date = DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Local);
+            var generator = new DateTimeGenerator();
+
+            Assert.NotEqual(date.Kind, generator.Kind);
+            Assert.Throws<ArgumentException>(
+                () => generator.NextLessThan(date)
+            );
+        }
+
+        [Fact]
+        public void NextLessThanOrEqualTo_MismatchedKind() {
+            var date = DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Local);
+            var generator = new DateTimeGenerator();
+
+            Assert.NotEqual(date.Kind, generator.Kind);
+            Assert.Throws<ArgumentException>(
+                () => generator.NextLessThanOrEqualTo(date)
+            );
+        }
+
+        [Fact]
+        public void NextGreaterThan_MismatchedKind() {
+            var date = DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Local);
+            var generator = new DateTimeGenerator();
+
+            Assert.NotEqual(date.Kind, generator.Kind);
+            Assert.Throws<ArgumentException>(
+                () => generator.NextGreaterThan(date)
+            );
+        }
+
+        [Fact]
+        public void NextGreaterThanOrEqualTo_MismatchedKind() {
+            var date = DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Local);
+            var generator = new DateTimeGenerator();
+
+            Assert.NotEqual(date.Kind, generator.Kind);
+            Assert.Throws<ArgumentException>(
+                () => generator.NextGreaterThanOrEqualTo(date)
+            );
+        }
+
 
     }
 
